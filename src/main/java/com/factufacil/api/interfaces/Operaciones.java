092a712633e4abd004328786194d8a39ba3ed810
Ronaldo <arolpersonal@gmail.com>
2023-05-18 19:24:40 -0500

    Se creo los metodos de la interfaz

---
+++ src/main/java/com/factufacil/api/interfaces/Operaciones.java
@@ -0,0 +1,17 @@
+package com.factufacil.api.interfaces;
+
+import java.util.List;
+
+public interface Operaciones<T> {
+	
+	public abstract boolean agregar(T objeto);
+	
+	public abstract List<T> obtenerTodos();
+	
+	public abstract boolean eliminar(Long id);
+	
+	public abstract boolean actualizar(T objeto);
+	
+	public abstract T obtenerUno(Long id);
+	
+}
